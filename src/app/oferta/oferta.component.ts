747ba22ef0c4cb2930e02a806194b145ffe90e7c
Fernandes <andre.a.fernandes@capgemini.com>
2021-06-01 15:15:48 -0300

    implementação do snapshot capturando o id ativo no momento

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+//interface
+import { ActivatedRoute } from '@angular/router';
+
+
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  //
+  private route!: ActivatedRoute;
+
+  //instanciando no construtor o route para fazermos um snapshot da rota ativa no momento
+  constructor(route: ActivatedRoute) {
+      this.route = route
+   }
 
   ngOnInit(): void {
+    //recebendo o snapshot utilizando os parametros para indicar o que buscar
+    this.route.snapshot.params['id'];
   }
 
 }
